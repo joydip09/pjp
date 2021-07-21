c2612786d91009653aa51b002904c3545dcd95dc
C_Joydip <75995283+C-Joydip@users.noreply.github.com>
2021-07-21 20:53:35 +0600

    Add files via upload

---
+++ scripts/pjp.js
@@ -0,0 +1,31 @@
+function pics() {
+var pi=document.getElementById("pics")
+var po=document.getElementById("po")
+pi.style.display=""
+po.style.display="none"
+}
+var pic= new Array (
+"images/Araf.jpg","images/Mosiur.jpg","images/Raktim.jpg","images/Joydip.jpg"
+)
+var name= new Array (
+"Araf","Mosiur","Raktim","Joydip"
+)
+var num=0
+function prev() {
+var img = document.getElementById("img")
+num--
+if (num<0){
+num=0
+}
+img.src=pic[num]
+img.alt=name[num]
+}
+function next() {
+var img = document.getElementById("img")
+num++
+if (num>=pic.length){
+num=0
+}
+img.src=pic[num]
+img.alt=name[num]
+}
